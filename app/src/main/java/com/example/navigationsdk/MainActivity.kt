880630f59ae86d9faa4078afe92cdc24b3bad8b9
Pavlo Shakin <pavvvvvel>
2022-10-03 15:16:18 +0300

    * added map_id

--- app/src/main/java/com/example/navigationsdk/MainActivity.kt
+++ app/src/main/java/com/example/navigationsdk/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.navigationsdk
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -14,8 +11,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment!!.getMapAsync(this)
+        val mapFragment = MapFragment.newInstance(
+            GoogleMapOptions()
+                .mapId(resources.getString(R.string.map_id))
+        )
+        mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
