4bf049072998771b2749190e066449882254e8a6
Pavlo Shakin <pavvvvvel>
2022-10-05 12:00:36 +0300

    * implemented basic clusters functional

---
+++ app/src/main/java/com/example/navigationsdk/place/PlaceRender.kt
@@ -0,0 +1,40 @@
+package com.example.navigationsdk.place
+
+import android.content.Context
+import androidx.core.content.ContextCompat
+import com.example.navigationsdk.BitmapHelper
+import com.example.navigationsdk.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.ClusterManager
+import com.google.maps.android.clustering.view.DefaultClusterRenderer
+
+class PlaceRenderer(
+    private val context: Context,
+    map: GoogleMap,
+    clusterManager: ClusterManager<Place>
+) : DefaultClusterRenderer<Place>(context, map, clusterManager) {
+
+    private val bicycleIcon: BitmapDescriptor by lazy {
+        val color = ContextCompat.getColor(context,
+            R.color.purple_500
+        )
+        BitmapHelper.vectorToBitmap(
+            context,
+            R.drawable.ic_bicycle,
+            color
+        )
+    }
+
+    override fun onBeforeClusterItemRendered(item: Place, markerOptions: MarkerOptions) {
+        markerOptions.title(item.name)
+            .position(item.latLng)
+            .icon(bicycleIcon)
+    }
+
+    override fun onClusterItemRendered(clusterItem: Place, marker: Marker) {
+        marker.tag = clusterItem
+    }
+}
